4cae95c0afe5c495067baf9318adce1299be1fd8
Duongsiuuu <vuquangduong29042004@gmail.com>
2025-03-17 11:04:59 +0700

    update api register, login

--- src/main/java/org/example/Entity/User.java
+++ src/main/java/org/example/Entity/User.java
@@ -8,111 +8,41 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-
+import lombok.Getter;
+import lombok.Setter;
 import javax.persistence.*;
 import java.util.Set;
 
-@Entity
-@Table(name = "USER")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int userId;
-    private String name;
-    private String username;
-    private String password;
-    private String phone;
-    private String email;
-    private String gender;
-    private String roleCode;
+    @Entity
+    @Table(name = "USER")
+    public class User {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private int userId;
 
-    public int getUserId() {
-        return userId;
+        @Getter @Setter
+        private String name;
+        @Getter @Setter
+        private String username;
+        @Getter @Setter
+        private String password;
+        @Getter @Setter
+        private String phone;
+        @Getter @Setter
+        private String email;
+        @Getter @Setter
+        private String gender;
+        @Getter @Setter
+        private String roleCode;
+        @Getter @Setter
+        private String cccd;
+        @Getter @Setter
+        private String insuranceNumber;
+        @Getter @Setter
+        private String address;
+
+        @ManyToOne
+        @JoinColumn(name = "department_id")
+        @Getter @Setter
+        private Department department;
     }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getGender() {
-        return gender;
-    }
-
-    public void setGender(String gender) {
-        this.gender = gender;
-    }
-
-//    public Role getRole() {
-//        return role;
-//    }
-//
-//    public void setRole(Role role) {
-//        this.role = role;
-//    }
-
-    public Department getDepartment() {
-        return department;
-    }
-
-    public void setDepartment(Department department) {
-        this.department = department;
-    }
-
-    public String getRoleCode() {
-        return roleCode;
-    }
-
-    public void setRoleCode(String roleCode) {
-        this.roleCode = roleCode;
-    }
-
-//    @ManyToOne
-//    @JoinColumn(name = "role_code")
-//    private Role role;
-
-    @ManyToOne
-    @JoinColumn(name = "department_id")
-    private Department department;
-
-    // Getters and Setters
-}
